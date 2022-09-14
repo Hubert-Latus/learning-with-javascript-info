3b74174284aa35b47790f56344c95b9f68d6a334
Hubert <hubert.l@vp.pl>
2022-09-14 15:36:59 +0200

    2.13 Task 2/7

---
+++ 2.13-Loops-while-and-for/Which-values-does-the-while-show.js
@@ -0,0 +1,31 @@
+//For every loop iteration, write down which value it outputs and then compare it with the solution.
+
+//Both loops alert the same values, or not?
+
+//The prefix form ++i:
+
+let i = 0;
+while (++i < 5) alert( i );
+
+alert( i ); // 1
+
+alert( i ); // 2 
+
+alert( i ); // 3
+
+alert( i ); // 4
+
+//The postfix form i++
+
+//let i = 0;
+while (i++ < 5) alert( i );
+
+alert( i ); // 1
+
+alert( i ); // 2
+
+alert( i ); // 3
+
+alert( i ); // 4
+
+alert( i ); // 5
