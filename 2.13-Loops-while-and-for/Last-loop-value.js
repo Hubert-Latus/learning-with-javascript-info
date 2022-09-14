4ab338dc37d230fb75c4806aae5d7f8bbbbebbe5
Hubert <hubert.l@vp.pl>
2022-09-14 15:18:28 +0200

    2.13 Task 1/7

---
+++ 2.13-Loops-while-and-for/Last-loop-value.js
@@ -0,0 +1,20 @@
+// What is the last value alerted by this code? Why?
+
+let i = 3;
+
+while (i) {
+  alert( i-- ); // shows 3, decreases to 2.
+}
+
+alert( i-- ); // shows 2, decreases to 1
+
+alert( i-- ); // shows 1, decreases to 0
+
+// the last value shown is 1 becouse loop stops at 0.
+
+
+
+
+
+
+
