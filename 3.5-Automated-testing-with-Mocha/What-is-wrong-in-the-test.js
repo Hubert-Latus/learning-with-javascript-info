8889456d0f3cb18b49341a6af03f17d2dbae2bf5
Hubert <hubert.l@vp.pl>
2022-10-08 18:07:05 +0200

    3.5 task 1/1

--- 3.5-Automated-testing-with-Mocha/What-is-wrong-in-the-test.js
+++ 3.5-Automated-testing-with-Mocha/What-is-wrong-in-the-test.js
@@ -14,14 +14,14 @@
 
   describe ("Raises x to the power n", function() {
     it ("Raises 5 to the power 1", function() {
-        assert.equal(pow(x, 1), result);
+        assert.equal(pow(5, 1), 5);
     });
 
     it ("Raises 5 to the power 2", function() {
-        assert.equal(pow(x, 2), result);
+        assert.equal(pow(5, 2), 25);
     });
-    
+
     it ("Raises 5 to the power 3", function() {
-        assert.equal(pow(x, 3), result);
+        assert.equal(pow(5, 3), 125);
     });
  });
